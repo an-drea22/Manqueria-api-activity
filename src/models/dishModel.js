5d6aecad716a863d3cbecc1ef4e567619204d52d
andrea <@gmail.com>
2026-01-28 15:39:22 +0800

    Final submission for  API activity

---
+++ src/models/dishModel.js
@@ -0,0 +1,25 @@
+const dishes = [
+    {
+        id: 1,
+        name: 'Tacos al Pastor',
+        price: 12.99,
+        category: 'Main Course',
+        isVegetarian: false
+    },
+    {
+        id: 2,
+        name: 'Enchiladas Verdes',
+        price: 10.99,
+        category: 'Main Course',
+        isVegetarian: true
+    },
+    {
+        id: 3,
+        name: 'Churros',
+        price: 5.99,
+        category: 'Dessert',
+        isVegetarian: true
+    }
+];
+
+module.exports = dishes;
